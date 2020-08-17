03311bf0e05b29b00e9473bbf79050d836704929
Guilherme Augusto <guilherme.augusto9@outlook.com>
2020-08-17 14:37:55 -0300

    Update README.md

---
+++ Book 1 - Learn Python Hard Way/ex1.py
@@ -0,0 +1,7 @@
+print('Hello Word!')
+print('Hello again!')
+print('I like to typing this.')
+print ('this is fun.')
+print('Yay! Printing.')
+print("I'd much rather than 'not.")
+print('I "said" do not touch this.')
